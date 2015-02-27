4b78edd3f3a02071d620c57e59164d1a4a4426ba
Eric Draut <edraut@gmail.com>
2015-02-27 11:18:48 -0700

    fixed bug in listener_hash helper

--- lib/foreign_office/foreign_office_helper.rb
+++ lib/foreign_office/foreign_office_helper.rb
@@ -11,12 +11,11 @@ module ForeignOffice
       data_attrs += " data-sub-type=#{subtype}" if subtype
       data_attrs
     end
-  end
 
-  def listener_hash(resource, key, reveal_hide: false)
-    hash = {listener: true, channel: resource.class.name + resource.id.to_s, key: key}
-    hash[:reveal_hide] = true if reveal_hide
-    hash
+    def listener_hash(resource, key, reveal_hide: false)
+      hash = {listener: true, channel: resource.class.name + resource.id.to_s, key: key}
+      hash[:reveal_hide] = true if reveal_hide
+      hash
+    end
   end
-
 end
