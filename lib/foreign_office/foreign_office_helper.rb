1bb7a9659c51eb428c2e8087626cc60d7c05ecbf
Eric Draut <edraut@gmail.com>
2015-03-06 10:15:09 -0700

    improved listner_attrs signature

--- lib/foreign_office/foreign_office_helper.rb
+++ lib/foreign_office/foreign_office_helper.rb
@@ -1,14 +1,14 @@
 module ForeignOffice
   module ForeignOfficeHelper
-    def listener_attrs(resource, key, subtype: nil)
+    def listener_attrs(resource, key, reveal_hide: false)
       data_attrs = "data-listener=true data-channel=#{resource.class.name}#{resource.id} data-key=#{key}"
-      data_attrs += " data-sub-type=#{subtype}" if subtype
+      data_attrs += " data-reveal-hide=true" if reveal_hide
       data_attrs
     end
 
-    def listener_attrs_raw(channel, key, subtype: nil)
+    def listener_attrs_raw(channel, key, reveal_hide: false)
       data_attrs = "data-listener=true data-channel=#{channel} data-key=#{key}"
-      data_attrs += " data-sub-type=#{subtype}" if subtype
+      data_attrs += " data-reveal-hide=true" if reveal_hide
       data_attrs
     end
 
